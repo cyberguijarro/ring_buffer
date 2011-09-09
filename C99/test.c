56c9e88a27262e0a07a48804f86d74baa56035b1
Emilio Guijarro <cyberguijarro@gmail.com>
2011-09-09 09:51:44 +0200

    Test suite extended to cover the available() function in depth.

--- C99/test.c
+++ C99/test.c
@@ -8,23 +8,46 @@ static void simple() {
     unsigned char foo1 = 0xDE;
     unsigned short foo2 = 0xDEAD;
     unsigned int foo4 = 0xDEADFACE;
+    size_t read, write, rewind;
 
     assert(RING_BUFFER_SUCCESS == ring_buffer_create(&buffer, 8, 2));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 0) && (write == 6) && (rewind == 0));
+
     assert(RING_BUFFER_UNDERFLOW == ring_buffer_rewind(buffer, 2));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 0) && (write == 6) && (rewind == 0));
+
     assert(RING_BUFFER_SUCCESS == ring_buffer_write(buffer, &foo1, 1));
     assert(RING_BUFFER_SUCCESS == ring_buffer_write(buffer, &foo2, 2));
     assert(RING_BUFFER_OVERFLOW == ring_buffer_write(buffer, &foo4, 4));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 3) && (write == 3) && (rewind == 0));
+
     assert((RING_BUFFER_SUCCESS == ring_buffer_read(buffer, &foo1, 1)) && (foo1 == 0xDE));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 2) && (write == 4) && (rewind == 1));
+
     assert((RING_BUFFER_SUCCESS == ring_buffer_write(buffer, &foo4, 4)));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 6) && (write == 0) && (rewind == 1));
+
     assert((RING_BUFFER_SUCCESS == ring_buffer_read(buffer, &foo2, 2)) && (foo2 == 0xDEAD));
     assert((RING_BUFFER_SUCCESS == ring_buffer_read(buffer, &foo4, 4)) && (foo4 == 0xDEADFACE));
     assert((RING_BUFFER_UNDERFLOW == ring_buffer_read(buffer, &foo4, 4)) && (foo4 == 0xDEADFACE));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 0) && (write == 6) && (rewind == 2));
+    
     assert(RING_BUFFER_SUCCESS == ring_buffer_write(buffer, &foo2, 2));
     assert(RING_BUFFER_SUCCESS == ring_buffer_write(buffer, &foo4, 4));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 6) && (write == 0) && (rewind == 2));
+    
     assert((RING_BUFFER_SUCCESS == ring_buffer_read(buffer, &foo2, 2)) && (foo2 == 0xDEAD));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 4) && (write == 2) && (rewind == 2));
+    
     assert(RING_BUFFER_SUCCESS == ring_buffer_write(buffer, &foo1, 1));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 5) && (write == 1) && (rewind == 2));
+
     assert(RING_BUFFER_SUCCESS == ring_buffer_rewind(buffer, 2));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 7) && (write == 1) && (rewind == 0));
+
     assert((RING_BUFFER_SUCCESS == ring_buffer_read(buffer, &foo2, 2)) && (foo2 == 0xDEAD));
+    assert((RING_BUFFER_SUCCESS == ring_buffer_available(buffer, &read, &write, &rewind)) && (read == 5) && (write == 1) && (rewind == 2));
+
     assert(RING_BUFFER_SUCCESS == ring_buffer_destroy(buffer));
 }
 
